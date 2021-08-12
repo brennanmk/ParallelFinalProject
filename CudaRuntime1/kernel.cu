3733a28f7b8243b36ac12b465c8e66805414993d
brennan <brennanmk2200@gmail.com>
2021-08-12 16:43:21 -0400

    Got code working

--- CudaRuntime1/kernel.cu
+++ CudaRuntime1/kernel.cu
@@ -4,42 +4,84 @@
 #include <math.h>
 #include <stdlib.h>
 #include "cuda_runtime.h"
+#include <curand.h>
+#include <curand_kernel.h>
 #include "device_launch_parameters.h"
 #include <stdio.h>
 
-cudaError_t addWithCuda(int N, int a, int b, long double *total);
 
-
-__global__ void addKernel(int N, int a, int b, long double *total)
+__global__ void monteCarlo(long timeVal, int N, int a, int b, long double* answer)
 {
-    long double temp = 0;
-    int i = threadIdx.x;
+	long double temp = 0;
+	int index = threadIdx.x + blockIdx.x * blockDim.x;
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-    for (i = 0; i < N; i++) //perform provided calculations N times (add to total each time)
-    {
-        int numGen = (rand() % (b - a + 1)) + a;
+	long double F; //variable for final integral
+	int count = 0;
 
-        long double powerOf = (-1 * powl(numGen, 2) / 2);
-        temp += ((1 / sqrtl(2 * 3.14)) * ((powl(2.718, powerOf))));
-    }
 
-    *total += temp;
+
+	curandState_t state;
+	curand_init((timeVal * index), /* the seed controls the sequence of random values that are produced */
+		0, /* the sequence number is only important with multiple cores */
+		0, /* the offset is how much extra we advance in the sequence for each call, can be 0 */
+		&state);
+
+	if (i < N) {
+		int numGen = (curand(&state) % (b - a + 1)) + a;
+		count++;
+		double powerOf = (-1 * pow(numGen, 2) / 2);
+
+		answer[i] = ((1 / sqrt(2 * 3.14)) * ((pow(2.718, powerOf))));
+
+	}
+
 }
 
-int main(int argc, char* argv[])
-{
-    long double *total; //variable to store total
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-    int N = atoi(argv[3]);
+int main() {
+	int a = -5;
+	int b = 5;
+	int N = 100000000;
+	long timeVal = time(NULL);
+	double timeAvg;
 
-    cudaMallocManaged(&total, sizeof(long double));
+	int size = N * sizeof(long double);
 
-    // Add vectors in parallel.
-    addKernel << <1, 256 >> > (N, a, b, total);
+	long double* total = 0;
+	long double* d_total;
+	long double F = 0;
+	long double temp = 0;
 
 
-    printf("%lf", total);
+	clock_t startTime = clock(); //record start time (function found from https://en.cppreference.com/w/c/chrono/clock_t)
 
-    return 0;
+	total = (long double*)malloc(size);
+	cudaMalloc((void**)&d_total, size);
+
+	int nblocks = (N + 511) / 512;
+
+
+	cudaMemcpy(d_total, total, size, cudaMemcpyHostToDevice);
+
+	monteCarlo <<<nblocks, 512 >>> (timeVal, N, a, b, d_total);
+
+	cudaMemcpy(total, d_total, size, cudaMemcpyDeviceToHost);
+	
+	for (int i = 0; i < N; i++) {
+		temp += total[i];
+
+	}
+	F = (((long double)b - (long double)a) / (long double)N) * temp;
+
+
+	clock_t endTime = clock(); //end time (function found from https://en.cppreference.com/w/c/chrono/clock_t)
+	timeAvg = ((double)(endTime - startTime)) / CLOCKS_PER_SEC; //add the elasped time to timeAvg
+
+
+	printf("%lf\n", F);
+	printf("Run Time = %fs\n", timeAvg); //Print average time
+
+	cudaFree(d_total);
+
+	return 0;
 }
